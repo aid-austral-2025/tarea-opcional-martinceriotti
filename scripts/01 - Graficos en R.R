fd445a5087c99f67299eb377f755873c89233240
mceriotti <martinceriotti@gmail.com>
2025-10-04 17:41:00 -0300

    Tareas completadas hasta el pedido de "Escritura matemática".

---
+++ scripts/01 - Graficos en R.R
@@ -0,0 +1,34 @@
+library(ggplot2)
+library(readr)
+library(tidyverse)
+
+data <- read_csv("./data/sector_tecnologico_cie_2011-2015.csv")
+
+data$`Base Imponible (millones de pesos corrientes)` <- gsub(",", ".", data$`Base Imponible (millones de pesos corrientes)`)
+data$`Base Imponible (millones de pesos corrientes)` <- as.numeric(data$`Base Imponible (millones de pesos corrientes)`)
+
+boxplot(data$`Base Imponible (millones de pesos corrientes)`)
+table(data$Sector)
+barplot(table(data$Sector))
+
+ggplot(data, aes(x=data$`2º apertura`, y=data$`Base Imponible (millones de pesos corrientes)`)) + 
+  xlab("Base Imponible") +
+  ylab("Segunda Apertura") +
+  geom_bar(stat = "identity") +
+  coord_flip()
+
+
+
+#table(data$`2º apertura`, data$`Base Imponible (millones de pesos corrientes)`)
+data %>%
+  group_by(`1º apertura`) %>%
+  summarise(suma_valor = sum(`Base Imponible (millones de pesos corrientes)`))
+
+
+
+tabla <- data %>%
+  group_by(`1º apertura`) %>%
+  summarise(suma_valor = sum(`Base Imponible (millones de pesos corrientes)`))
+
+library(knitr)
+kable(tabla, escape = FALSE)
